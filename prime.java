39a4a289cc7b914fc161f3e5e7fc122b388f98c2
Ajay <sivvaajay1739@gmail.com>
2024-12-05 20:26:41 +0530

    done changes

---
+++ prime.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class prime {
+    //sieve of eratosthenes
+    public static int[] isPrime(int n)
+    {
+        int[] parr= new int[n+1];
+        for(int i=0;i<=n;i++)
+        {
+            parr[i] = 1;
+        }
+        for(int i=2;i<Math.sqrt(n);i++)
+        {
+            for(int j = i*i;j<=n;j+=i)
+            {
+                parr[j] = 0;
+            }
+        }
+        return parr;
+    }
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] primy = isPrime(n); 
+        for(int i=1;i<n;i++)
+        {
+            if(primy[i]==1)
+            System.out.print(i+" ");
+        }
+        in.close();
+    }
+    
+}
