9bbd5cbf4178c986165e276a22ea4bee1634d5b5
Ajay6908 <153096303+Ajay6908@users.noreply.github.com>
2025-08-13 22:30:14 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (50.70%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,19 +1,15 @@
 class Solution {
-    public static int recursion(int n,int[] dp)
-    {
-        if(n==1||n==0)
-        return 1;
-        if(dp[n]!=-1)
-        return dp[n];
-        dp[n]=recursion(n-1,dp)+recursion(n-2,dp);
-        return dp[n];
-    }
+    
     public int climbStairs(int n) {
       
         int dp[] = new int[n+1];
-          Arrays.fill(dp,-1);
-        int ans = recursion(n,dp);
-        return ans;
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2]; 
+        }
+        return dp[n];
         
     }
 }
