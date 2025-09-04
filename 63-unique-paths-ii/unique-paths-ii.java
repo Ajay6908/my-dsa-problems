e68dba2ea59a6322a3dcd799fc98cc1e0d36e374
Ajay6908 <153096303+Ajay6908@users.noreply.github.com>
2025-09-04 16:30:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (97.13%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,40 +1,35 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] arr) {
+    public static int recur(int[][] mat,int m,int n,int[][] dp)
+    {
+        if(m<0||n<0)
+        return 0;
+         if(dp[m][n]!=-1)
+        return dp[m][n];
+        if(m==0&&n==0)
+        {
+            return 1;
+        }
+       
         
-        if (arr[0][0] == 1) return 0;
-        for(int i=0;i<arr.length;i++)
+        if(mat[m][n]==1)
         {
-            for(int j=0;j<arr[0].length;j++)
-            {
-                if(arr[i][j] == 1)
-                arr[i][j] = -1;
-            }
+            return 0;
         }
-
-        int m = arr.length;
-        int n = arr[0].length;
-        int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++)
+        int up = recur(mat,m-1,n,dp);
+        int left = recur(mat,m,n-1,dp);
+        return dp[m][n]=up+left;
+    }
+    public int uniquePathsWithObstacles(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        if (mat[0][0] == 1 || mat[m - 1][n - 1] == 1) return 0;
+        int dp[][] = new int[m][n];
+        for(int[] a:dp)
         {
-            for(int j=0;j<n;j++)
-            {
-                 if (arr[i][j] == -1) {
-                    dp[i][j] = 0;
-                    continue;
-                }
-                if(i==0&&j==0)
-                dp[i][j] = 1;
-              
-                else
-                {
-                    int up = (i>0)?dp[i-1][j]:0;
-                    int left = (j>0)?dp[i][j-1]:0;
-                    dp[i][j] = up+left;
-                }
-            }
+            Arrays.fill(a,-1);
         }
-        return dp[m-1][n-1];
-
+        int ans = recur(mat,m-1,n-1,dp);
+        return ans;
         
     }
 }
