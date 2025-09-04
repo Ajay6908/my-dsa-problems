7c302255262c9504c1c92177606856b5d35a292b
Ajay6908 <153096303+Ajay6908@users.noreply.github.com>
2025-09-04 18:19:02 +0530

    Time: 5 ms (97.07%) | Memory: 41.7 MB (50.91%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,25 +1,28 @@
 class Solution {
-    public static int checkPalindrome(String s, int left, int right) {
+    public static int ispalindrome(String s,int left,int right)
+    {
         int count = 0;
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            count++;  // found one palindrome
-            left--;
-            right++;
+        while(left>=0&&right<s.length()&&s.charAt(left)==s.charAt(right))
+        {
+          
+                count++;
+               
+                
+            
+             left--;
+                right++;
         }
         return count;
     }
-
     public int countSubstrings(String s) {
-        if (s == null || s.length() == 0) return 0;
-
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            // odd length palindromes
-            count += checkPalindrome(s, i, i);
-
-            // even length palindromes
-            count += checkPalindrome(s, i, i + 1);
+        int n = s.length();
+        int cnt = 0;
+        for(int i=0;i<n;i++)
+        {
+            cnt+=ispalindrome(s,i,i);
+            cnt+=ispalindrome(s,i,i+1);
         }
-        return count;
+        return cnt;
+        
     }
 }
