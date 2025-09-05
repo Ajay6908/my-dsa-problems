5ac3c203441d725348e44d92b8309aa91624dab5
Ajay6908 <153096303+Ajay6908@users.noreply.github.com>
2025-09-05 18:50:47 +0530

    Time: 4 ms (49.33%) | Memory: 52.6 MB (9.45%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -9,6 +9,30 @@ class Solution {
         dfs(r,c-1,m,n,grid);
         dfs(r-1,c,m,n,grid);
     }
+    public static void bfs(int rr,int cc,int m,int n,char[][] grid)
+    {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{rr,cc});
+        int trax[] = {1,0,-1,0};
+        int tray[] = {0,1,0,-1};
+        while(!q.isEmpty())
+        {
+            int row = q.peek()[0];
+            int col = q.peek()[1];
+            q.poll();
+            for(int i=0;i<4;i++)
+            {
+                int r = row + trax[i];
+                int c = col + tray[i];
+                if(r>=0&&r<m&&c>=0&&c<n&&grid[r][c]=='1')
+                {
+                    grid[r][c] = '0';
+                    q.offer(new int[]{r,c});
+                }
+            }
+        }
+        
+    }
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -20,7 +44,7 @@ class Solution {
                 if(grid[i][j] == '1')
                 {
                     cnt++;
-                    dfs(i,j,m,n,grid);
+                    bfs(i,j,m,n,grid);
                 }
             }
         }
