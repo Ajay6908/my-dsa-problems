af0607edba8523a9f90e5f5050c1d1c543512c8b
Ajay6908 <153096303+Ajay6908@users.noreply.github.com>
2025-09-07 15:16:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (50.64%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,35 +1,31 @@
 class Solution {
-    public static int recur(int[][] mat,int m,int n,int[][] dp)
-    {
-        if(m<0||n<0)
-        return 0;
-         if(dp[m][n]!=-1)
-        return dp[m][n];
-        if(m==0&&n==0)
-        {
-            return 1;
-        }
-       
-        
-        if(mat[m][n]==1)
-        {
-            return 0;
-        }
-        int up = recur(mat,m-1,n,dp);
-        int left = recur(mat,m,n-1,dp);
-        return dp[m][n]=up+left;
-    }
     public int uniquePathsWithObstacles(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
-        if (mat[0][0] == 1 || mat[m - 1][n - 1] == 1) return 0;
         int dp[][] = new int[m][n];
-        for(int[] a:dp)
+        for(int i=0;i<m;i++)
         {
-            Arrays.fill(a,-1);
+            for(int j=0;j<n;j++)
+            {
+                if(i==0&&j==0)
+                dp[i][j] = 1;
+                if(mat[i][j] == 1)
+                {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if(i>0)
+                {
+                    dp[i][j] += dp[i-1][j];
+                }
+                if(j>0)
+                {
+                    dp[i][j] +=dp[i][j-1];
+                }
+                
+            }
         }
-        int ans = recur(mat,m-1,n-1,dp);
-        return ans;
+        return dp[m-1][n-1];
         
     }
 }
