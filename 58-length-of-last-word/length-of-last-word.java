c9ce707360d23775596c34247b05780cada282e0
Ajay6908 <153096303+Ajay6908@users.noreply.github.com>
2025-12-21 12:57:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (10.05%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,15 +1,29 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-         s=s.trim();
-         int cnt = 0;
-         int n = s.length();
-         for(int i = n-1;i>=0;i--)
-         {
-            if(s.charAt(i)==' ')
-            break;
-            cnt++;
-         }
-         return cnt;
+        int n = s.length();
+        if(n==1)
+        {
+            return s.charAt(0)==' '?0:1;
+        }
+        int cnt = 0;
+        boolean flag = false;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s.charAt(i)==' '&&!flag)
+            {
+                continue;
+            }
+            if(s.charAt(i)!=' ')
+            {
+                flag = true;
+                cnt++;
+            }
+            else
+            {
+                return cnt;
+            }
+        }
+        return cnt;
         
     }
 }
